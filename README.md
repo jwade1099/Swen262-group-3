5f32fd8e59f2cda7d8444400479bed4096643ef3
Justin Wade <jpwade109@gmail.com>
2023-02-28 01:35:59 -0500

    user class

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Swen 262 group 3 NutriApp
 
 Justin Wade
+
+
---
+++ User.java
@@ -0,0 +1,45 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class User {
+
+    private String name;
+    private int height; // inches
+    private int weight; //pounds
+    private String birthday; // mm/dd/yyyy
+    private int age;
+
+
+    public User(String name, int height, int weight, String birthday) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.birthday = birthday;
+
+
+
+    }
+
+    private int calculateAge() {
+        // String[] elements = birthday.split("/");
+
+        // int day = Integer.parseInt(elements[0]);
+
+        // Date current = new java.util.Date();
+
+        // Calendar cal = new Calendar();
+
+
+        // current.compareTo(current)
+
+
+        return 1;
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}

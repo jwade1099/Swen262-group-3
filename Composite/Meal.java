6f587d26a3cc0820d13e071b2ede4eb33742687d
Justin Wade <jpwade109@gmail.com>
2023-04-03 16:28:56 -0400

    calorie function for meals

--- Composite/Meal.java
+++ Composite/Meal.java
@@ -1,8 +1,17 @@
 package Composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal implements Food {
+    private double calories;
 
     private List<Food> foods;
+
+    public Meal() {
+        this.calories = 0;
+        this.foods = new ArrayList<>();
+    }
 
     @Override
     public double getCalories() {

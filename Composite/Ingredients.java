54cad4ec23d326cd8ea6e07d1decc51ec84a8612
Justin Wade <jpwade109@gmail.com>
2023-04-03 11:48:05 -0400

    composite pattern push 2

---
+++ Composite/Ingredients.java
@@ -0,0 +1,14 @@
+package Composite;
+
+public class Ingredients implements Food {
+    private double calories;
+    private int stock;
+
+    @Override
+    public double getCalories() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
+
---
+++ Composite/Recipe.java
@@ -0,0 +1,11 @@
+package Composite;
+
+public class Recipe implements Food {
+
+    @Override
+    public double getCalories() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}

a3d3661a99303c896de8df2400af999b067c5223
Justin Wade <jpwade109@gmail.com>
2023-04-03 16:02:05 -0400

    composite push 3

---
+++ Composite/Meal.java
@@ -0,0 +1,13 @@
+package Composite;
+
+public class Meal implements Food {
+
+    private List<Food> foods;
+
+    @Override
+    public double getCalories() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}

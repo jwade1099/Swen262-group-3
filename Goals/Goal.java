90c6266ccd8f30fb33f4fa2a9ed35dd3bf8e2964
Paolo Pop <pp6876@rit.edu>
2023-04-03 22:01:19 -0400

    Added user's weight tracking, methods for state changes and calorie setting.

--- Goals/Goal.java
+++ Goals/Goal.java
@@ -1,33 +1,129 @@
 package Goals;
 
+import source.User;
+
 public class Goal {
     private GoalsState state;
-    private int initialWeight;
-    private int weightGoal;
-    public int targetCalories;
-//    public ExerciseIntensity intensity;
+    private final User user;
 
-    public Goal(source.User user, int weightGoal, GoalsState state) {
+    /**
+     * user's initial weight.
+     */
+    private double initialWeight;
+
+    /**
+     * user's current weight.
+     */
+    private double currentWeight;
+
+    /**
+     * user's weight goal.
+     */
+    private double weightGoal;
+
+    /**
+     * user's daily target calories before workout and goal change.
+     */
+    private double baseCalories;
+
+    /**
+     * user's daily target calories.
+     */
+    private double targetCalories;
+//    private double workoutCalories;
+
+
+    public Goal(source.User user, int age, int weightGoal) {
+        this.user = user;
+        this.initialWeight = user.getWeight();
+        this.currentWeight = user.getWeight();
+        this.weightGoal = weightGoal;
+
+        setBaseCalories(age);
+        setCalories();
+    }
+
+
+    /**
+     * Set the user's current weight as the {@link #currentWeight} and {@link #initialWeight}, and the
+     *
+     * @param state to set as new {@link #state}.
+     */
+    public void setState(GoalsState state) {
         this.state = state;
         this.initialWeight = user.getWeight();
+        this.currentWeight = user.getWeight();
+    }
+
+
+    /**
+     * Set the {@link #targetCalories} based on the current state.
+     */
+    public void setCalories() {
+        state.handleSetCalories(baseCalories);
+    }
+
+
+    /**
+     * @param targetCalories to set as the new {@link #targetCalories}.
+     */
+    public void setTargetCalories(double targetCalories) {
+        this.targetCalories = targetCalories;
+    }
+
+
+    /**
+     * @param age used to determine the {@link #baseCalories}.
+     */
+    public void setBaseCalories(int age) {
+        this.baseCalories = (9.99 * currentWeight) + (6.25 * user.getHeight()) - (4.92 * age) - 83;
+    }
+
+
+    /**
+     * @param weightGoal to set as the new {@link #weightGoal}.
+     */
+    public void setWeightGoal(double weightGoal) {
         this.weightGoal = weightGoal;
     }
 
-    public void setTargetCalories(int targetCalories) {
-        state.handleSetCalories();
+
+    /**
+     * @param currentWeight to set as the new {@link #currentWeight}.
+     */
+    public void setCurrentWeight(double currentWeight) {
+        this.currentWeight = currentWeight;
     }
 
-    public void setState(GoalsState state) {
-        this.state = state;
+
+    /**
+     * @return the current {@link #weightGoal}.
+     */
+    public double getWeightGoal() {
+        return weightGoal;
     }
 
-    public int getWeightDifference() {
-        return 0;
+
+    /**
+     * @return the current {@link #targetCalories}.
+     */
+    public double getTargetCalories() {
+        return targetCalories;
     }
 
-//    public setIntensity(ExerciseIntensity intensity){
-//        this.intensity = intensity;
-//    }
+
+    /**
+     * @return the user's {@link #currentWeight}
+     */
+    public double getCurrentWeight() {
+        return this.currentWeight;
+    }
 
 
+    /**
+     * @return the difference between the {@link #currentWeight} and the {@link #initialWeight}.
+     */
+    public double getWeightDifference() {
+        return currentWeight - initialWeight;
+    }
 }

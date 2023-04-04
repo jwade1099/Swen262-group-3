41cacd62e8724bbf39cf5faf641c5a8eaba8cdec
Paolo Pop <pp6876@rit.edu>
2023-04-03 22:05:50 -0400

    Added logic for handling calorie setting and state switching for different goal states.

--- Goals/GainWeight.java
+++ Goals/GainWeight.java
@@ -1,16 +1,20 @@
 package Goals;
 
 public class GainWeight implements GoalsState {
-    private Goal context;
-    // Todo find formula for determining target calories from current weight and weight goal.
-    int calories = 100;
+    private final Goal context;
+    int surplus = 500;
 
     public GainWeight(Goal context) {
         this.context = context;
     }
 
+
     @Override
-    public void handleSetCalories() {
-        context.targetCalories = calories;
+    public void handleSetCalories(double baseCalories) {
+        if (context.getCurrentWeight() >= context.getWeightGoal()) {
+            context.setState(new MaintainWeight(context));
+            context.setCalories();
+        }
+        context.setTargetCalories(baseCalories + surplus);
     }
 }
--- Goals/MaintainWeight.java
+++ Goals/MaintainWeight.java
@@ -1,14 +1,22 @@
 package Goals;
 
 public class MaintainWeight implements GoalsState {
-    private Goal context;
+    private final Goal context;
 
     public MaintainWeight(Goal context) {
         this.context = context;
     }
 
     @Override
-    public void handleSetCalories() {
-
+    public void handleSetCalories(double baseCalories) {
+        double weightDifference = context.getWeightDifference();
+        if (weightDifference >= 2.27) {
+            context.setState(new LoseWeight(context));
+            context.setCalories();
+        } else if (weightDifference <= - 2.27){
+            context.setState(new GainWeight(context));
+            context.setCalories();
+        }
+            context.setTargetCalories(baseCalories);
     }
 }

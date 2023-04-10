4851b993ec66dd2e97c0ff7b352ef494a06fa37d
kalen-luraschi <kalenluraschi@gmail.com>
2023-04-09 23:27:00 -0400

    finally got vs code to work and started workouts

---
+++ Workouts/High.java
@@ -0,0 +1,30 @@
+package Workouts;
+
+public class High implements WorkoutStrat{
+    private int duration;
+    private double intensity;
+    public String time_date;
+
+    @Override
+    public int get_duration() {
+        return duration;
+    }
+
+    @Override
+    public double get_intensity() {
+        return intensity;
+    }
+
+
+    @Override
+    public String get_time_date() {
+        return time_date;
+    }
+
+    @Override
+    public void add_calories_target(int calories) {
+        source.User.add_calories_target(calories);
+
+    }
+    
+}
---
+++ Workouts/Workout.java
@@ -0,0 +1,23 @@
+package Workouts;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Workout {
+    //private WorkoutStrat start;
+    private int duration;
+    private double intensity;
+    public String time_date;
+
+    public Workout(int duration, double intensity){
+        this.duration = duration;
+        this.intensity = intensity;
+        /*for date */
+            Date current_date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentDateTime = dateFormat.format(current_date);
+        /*for date */
+        this.time_date = currentDateTime;
+    }
+
+}

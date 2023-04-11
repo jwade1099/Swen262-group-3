d932a620fe0c102c9704ab799e2ce657b8cb76c5
Justin Wade <jpwade109@gmail.com>
2023-04-10 21:17:05 -0400

    composite

--- Composite/Composite.java
+++ Composite/Composite.java
@@ -1,9 +1,16 @@
 package Composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Composite {
     public static void main(String[] args) {
         Food ingredient1 = new Ingredients(100, 20);
         Food ingredient2 = new Ingredients(200, 30);
+
+
+        List<Food> foodsList = new ArrayList<>();
+        
 
         //Food recipe1 = new Recipe()
     

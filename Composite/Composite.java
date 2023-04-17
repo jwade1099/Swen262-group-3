8334a8c34790353baff755f84552ca37622cbe3a
Justin Wade <jpwade109@gmail.com>
2023-04-17 17:07:16 -0400

    started to work with json objects

--- Composite/Composite.java
+++ Composite/Composite.java
@@ -1,7 +1,13 @@
 package Composite;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import com.fasterxml.jackson.core.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
 
 public class Composite {
@@ -11,6 +17,36 @@ public class Composite {
 
 
         List<Food> foodsList = new ArrayList<>();
+
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("Composite/foods.json"));
+            
+            JSONObject jsonObject = (JSONObject)obj;
+
+            System.out.println(jsonObject.toString());
+            
+
+
+            String name = (String)jsonObject.get("users.Name");
+            System.out.println(name);
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+
+
+
+        
+
+        
+
+        
+        
 
         
         
